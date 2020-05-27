ca0f5fcd79bb6476fb76a085b868d13748fe6d8c
TANG WENTAO <49475716+tang132442@users.noreply.github.com>
2020-05-27 16:57:02 +0900

    Add files via upload

---
+++ shopping/php/wantedlist.php
@@ -0,0 +1,42 @@
+<?php
+header('Content-Type:application/json; charset=utf-8');
+header("Access-Control-Allow-OriGIN:*");
+$parameter=$_POST['parameter'];
+$id=$_POST['id'];
+$dbserver="54.250.5.253";
+$dbuser="pbl3";
+$dbpwd="123456";
+$database="shopping";
+$usename=$_POST['username'];
+$conn=new mysqli($dbserver,$dbuser,$dbpwd,$database);
+if($parameter==0){
+    $sql="SELECT * from Purchase where `user_Name`='".$_COOKIE["my_cookie"]."'";
+    $arr=array();
+    $result = mysqli_query($conn,$sql);
+    $nums=$result->num_rows;
+    for($i=0;$i<$nums;$i++){
+        $row=$result->fetch_assoc();
+        array_push($arr,array('purchase_Id'=>$row["purchase_Id"],'goods'=>$row["goods"],'budget'=>$row["budget"],'note'=>$row["note"]));
+    }
+    $msg=array
+    ( 
+      "Num"=>$nums,
+      "Content"=>$arr
+    );
+}
+if($parameter==1){
+    $sql="DELETE from Purchase where `purchase_Id`='".$id."'";
+    mysqli_query($conn,$sql);
+    $sql="SELECT * from Purchase where `purchase_Id`='".$id."'";
+    $result = mysqli_query($conn,$sql);
+    if($result->num_rows==0){
+        $msg="Success";
+    }
+    else{
+        $msg="faild";
+    }
+}
+$conn->close();
+exit(json_encode($msg));
+
+?>
