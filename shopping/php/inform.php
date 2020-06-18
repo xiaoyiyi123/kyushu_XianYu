40b889eafe5af22c0fb12eaad3b26024d23b5a4d
TANG WENTAO <49475716+tang132442@users.noreply.github.com>
2020-06-18 11:34:35 +0900

    Add files via upload
    在自己电脑上测试时，记得把头像所在文件夹的权限设置为可写。

--- shopping/php/inform.php
+++ shopping/php/inform.php
@@ -14,7 +14,7 @@ $sql="SELECT * from Users where `User_Name`='".$_COOKIE["my_cookie"]."'";
 $result = mysqli_query($conn,$sql);
 
 $row=$result->fetch_assoc();
-$arr=array('Name'=>$row["User_Name"],'Email'=>$row["EMail"],'Gender'=>$row["Gender"],'Campus'=>$row["Campus"],'Photo'=>$row["Photo"],'Introduction'=>$row["Introduction"]);
+$arr=array('Name'=>$row["User_Name"],'Email'=>$row["Email"],'Gender'=>$row["Gender"],'Campus'=>$row["Campus"],'Photo'=>$row["Photo"],'Introduction'=>$row["Introduction"]);
 
 $conn->close();
 exit(json_encode($arr));
