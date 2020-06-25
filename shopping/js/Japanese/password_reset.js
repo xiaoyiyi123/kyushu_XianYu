fb37c23259b85606e660ef4b12df645afae757f9
TANG WENTAO <49475716+tang132442@users.noreply.github.com>
2020-06-25 14:43:37 +0900

    Add files via upload

--- shopping/js/Japanese/password_reset.js
+++ shopping/js/Japanese/password_reset.js
@@ -5,42 +5,60 @@ $(function (){
     $("#main_ps2").css({display:"none"});
     $("#main_ps3").css({display:"none"});
 //输入邮箱发送验证码
-
+    
     $("#mailSubmit").click(function (){
-        var mail=$("#mail").val();
-        alert("メール送りました！")
-        console.log(mail.value);
-        
+        var mail;
+        mail=$("#mail").val();
+        //alert("メール送りました！")
+        //console.log(mail.value);
+        console.log(mail);
+        var language = 0; //ja = 0, ch = 1, en =2
+        var flag = 1;//flag=1:emaill sent;flag=2:password change
             $.ajax({
                 //请求地址
-                url:"../../php/password_reset_mail.php",
+                url:"../../php/password_reset.php",
                 //请求方式
                 type:'post',
                 //往服务器传递过去的参数
                 data:{
                     "mail":mail,
-                    
+                    "flag":flag,
+                    "language":language
                 },
                 
                 //成功的时候执行
-                success:function (result){
-                    console.log(result);
-                    var codeconfirm=$("#code").val();
-                    if(codeconfirm == result){
-                        alert("パスワードをリセットしてください")
-                        $("#main_ps1").css({display:"none"});
-                        $("#main_ps2").css({display:"block"});
-                    }
-                    else{
-                        alert("検証コードが間違いました")
+                success:function (str){
+                    console.log(str); 
+                    if(str==0)
+                    {
+                        alert("メールが間違いました！")
                         window.location.href="../../shop/Japanese/password_reset_J.html";
                     }
-
+                    else
+                    {
+                        var code;
+                        code=str;
+                        //console.log(code);
+                        $("#codeSubmit").click(function (){
+                        var codeconfirm=$("#code").val();
+                        //console.log(codeconfirm);
+                        //console.log(code);
+                        if(codeconfirm == code){
+                            alert("パスワードをリセットしてください")
+                            $("#main_ps1").css({display:"none"});
+                            $("#main_ps2").css({display:"block"});
+                        }
+                        else{
+                            alert("検証コードが間違いました")
+                            window.location.href="../../shop/Japanese/password_reset_J.html";
+                        }
+                        });
+                    }
                 } 
             });
     
            });
-
+          
     //下一步 
     // 确认重置
     $("#confirmPassword").click(function (){
@@ -89,8 +107,8 @@ $(function (){
         };
     
 
-    console.log("password:"+pwd);
-   
+    //console.log("password:"+pwd);
+    var flag=2;
     $.ajax({
         //请求地址
         url:"../../php/password_reset.php",
@@ -99,7 +117,8 @@ $(function (){
         //往服务器传递过去的参数
         data:{
             "pwd":pwd,
-           
+            "flag":flag,
+            "mail":mail
         },
         success:function (b){
             console.log(b);
